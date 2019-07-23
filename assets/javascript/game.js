715079327fef4e575b55d6cded2acc407ed48918
Helen Kim <Helenk490@gmail.com>
2019-07-22 22:06:33 -0400

    working on if statement for scores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,47 +8,53 @@ $(document).ready(function () {
     let totalScore = 0;
     $('.myTotalScore').text('0');
     $(".randomNumber").text(rndNum);
+    let winCount = 0;
+    let lossCount = 0;
+
+    let a = totalScore
+    let b = rndNum
+    
+    if (a === b) {
+        winCount++
+        $(".wins").text("Wins:" + winCount);
+    } else if (a>b) {
+        lossCount++
+        $(".loss").text("Loss:" + lossCount);
+    }
 
 
+    //  let newScore= $(".myTotalScore").val() + e.currentTarget.value
 
-       let winCount =0
-       let loseCount =0
- 
-//  let newScore= $(".myTotalScore").val() + e.currentTarget.value
-       
-//$attr("data-value")
-//   $('#btn1').click() same thing as below
- 
+    //$attr("data-value")
+    //   $('#btn1').click() same thing as below
+
+
     $("#btn1").on("click", function () {
         totalScore += button1
-        console.log (totalScore)
+        console.log(totalScore)
         $(".myTotalScore").text(totalScore);
     });
 
     $("#btn2").on("click", function () {
         totalScore += button2
-        console.log (totalScore)
+        console.log(totalScore)
         $(".myTotalScore").text(totalScore);
     });
 
     $("#btn3").on("click", function () {
         totalScore += button3
-        console.log (totalScore)
+        console.log(totalScore)
         $(".myTotalScore").text(totalScore);
     });
 
     $("#btn4").on("click", function () {
         totalScore += button4
-        console.log (totalScore)
+        console.log(totalScore)
         $(".myTotalScore").text(totalScore);
     });
+    
 
-    if (totalScore===rndNum) {
-        winCount++
-        console.log ("Wins" + winCount)
-        $('.wins').text("Wins"+ winCount)
-    }
-
-//(`Wins:${winCount}`)
+    //(`Wins:${winCount}`)
 
 });
+
