d5e426019094058e35ea2d5d7eeb197be92c4da2
Helen Kim <Helenk490@gmail.com>
2019-07-23 15:56:07 -0400

    resetGame function created and added to if statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,73 @@
 $(document).ready(function () {
 
-    const rndNum = Math.floor(Math.random() * 102) + 19;
-    const button1 = Math.floor(Math.random() * 12) + 1;
-    const button2 = Math.floor(Math.random() * 12) + 1;
-    const button3 = Math.floor(Math.random() * 12) + 1;
-    const button4 = Math.floor(Math.random() * 12) + 1;
+    let rndNum = Math.floor(Math.random() * 102) + 19;
+    let button1 = Math.floor(Math.random() * 12) + 1;
+    let button2 = Math.floor(Math.random() * 12) + 1;
+    let button3 = Math.floor(Math.random() * 12) + 1;
+    let button4 = Math.floor(Math.random() * 12) + 1;
     let totalScore = 0;
     $('.myTotalScore').text('0');
     $(".randomNumber").text(rndNum);
     let winCount = 0;
     let lossCount = 0;
 
-    let a = totalScore
-    let b = rndNum
-    
-    if (a === b) {
-        winCount++
-        $(".wins").text("Wins:" + winCount);
-    } else if (a>b) {
-        lossCount++
-        $(".loss").text("Loss:" + lossCount);
-    }
 
 
-    //  let newScore= $(".myTotalScore").val() + e.currentTarget.value
 
-    //$attr("data-value")
-    //   $('#btn1').click() same thing as below
+    function winLoss() {
+
+        if (totalScore === rndNum) {
+            winCount++;
+            $(".wins").text("Wins:" + winCount);
+            redoGame();
+
+        } else if (totalScore > rndNum) {
+            lossCount++;
+            $(".loss").text("Loss:" + lossCount);
+            redoGame();
+
+        }
+    };
+
+
+
+    function redoGame() {
+
+        rndNum = Math.floor(Math.random() * 102) + 19;
+        button1 = Math.floor(Math.random() * 12) + 1;
+        button2 = Math.floor(Math.random() * 12) + 1;
+        button3 = Math.floor(Math.random() * 12) + 1;
+        button4 = Math.floor(Math.random() * 12) + 1;
+        totalScore = 0;
+        $('.myTotalScore').text(totalScore);
+        $(".randomNumber").text(rndNum);
+
+    };
 
 
     $("#btn1").on("click", function () {
         totalScore += button1
         $(".myTotalScore").text(totalScore);
+        winLoss();
     });
 
     $("#btn2").on("click", function () {
         totalScore += button2
         $(".myTotalScore").text(totalScore);
+        winLoss();
     });
 
     $("#btn3").on("click", function () {
         totalScore += button3
         $(".myTotalScore").text(totalScore);
+        winLoss();
     });
 
     $("#btn4").on("click", function () {
         totalScore += button4
         $(".myTotalScore").text(totalScore);
+        winLoss();
     });
-    
 
-    //(`Wins:${winCount}`)
 
 });
-
